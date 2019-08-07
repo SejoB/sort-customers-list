8b671cf0d48f15bae42f8d6a2bbd5ba045bdf4a6
Sejo <sejo1981@gmail.com>
2019-08-07 16:18:20 +0300

    Updates

---
+++ precache-manifest.9698c04aab18a247e8245a69edebe166.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02290b2bcce4a541de66f1092e76ba76",
+    "url": "/sort-customers-list/index.html"
+  },
+  {
+    "revision": "106264a83b59796c1759",
+    "url": "/sort-customers-list/static/js/2.c2c334ae.chunk.js"
+  },
+  {
+    "revision": "bf9bd83b4bd25e71bff1",
+    "url": "/sort-customers-list/static/js/main.8a72b1a2.chunk.js"
+  },
+  {
+    "revision": "bb479ab59ccc2f5391d7",
+    "url": "/sort-customers-list/static/js/runtime~main.8f8a00a4.js"
+  }
+]);
