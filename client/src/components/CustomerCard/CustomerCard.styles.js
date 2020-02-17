d2d8bec51361473cda4d43d22af7afcdf39682cb
sejoB <sejo1981@gmail.com>
2020-02-17 14:26:24 +0200

    adding filter method, connected card to del api

--- client/src/components/CustomerCard/CustomerCard.styles.js
+++ client/src/components/CustomerCard/CustomerCard.styles.js
@@ -4,12 +4,19 @@ export const Container = styled.div`
 	display: flex;
 	flex-direction: column;
 	justify-content: space-evenly;
+	width: 100%;
 `
-export const Item = styled.div`
+export const Item = styled.li`
 	display: flex;
-	font-size: 1rem;
 	justify-content: space-between;
+	align-items: center;
+	font-size: 1rem;
+	white-space: nowrap;
 `
 export const Text = styled.div`
 	margin-left: 10px;
+`
+export const Text1 = styled.div`
+	font-size: 1rem;
+	padding: 4px;
 `
--- client/src/components/CustomerForm/CustomerForm.styles.js
+++ client/src/components/CustomerForm/CustomerForm.styles.js
@@ -2,76 +2,74 @@ import styled from 'styled-components'
 import { AddCircle, Close } from 'styled-icons/material'
 
 export const Form = styled.form`
-    display: ${props => props.displayForm};
-    flex-direction: column;
-    z-index: 100;
-    border: 1px solid #cccccc;
-    border-radius: 15px;
-    background-color: #ffff;
-    padding: 1rem;
-    transition: display 2s;
-`
-export const Input = styled.input`
-    border-radius: 3px;
-    border: 1px solid #cccccc;
-    font-size: 1.5rem;
-    padding: 4px;
-    margin-left: 1.5rem;
-    &:focus {
-        border-color: #1da0ff;
-    }
+	display: ${props => props.displayForm};
+	flex-direction: column;
+	z-index: 100;
+	border: 1px solid #cccccc;
+	border-radius: 15px;
+	background-color: #ffff;
+	padding: 1rem;
 `
 export const Label = styled.label`
-    margin: 1rem;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    font-size: 1.5rem;
-    color: darkgray;
-    white-space: nowrap;
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	margin: 1rem;
+	font-size: 1.5rem;
+	color: darkgray;
+	white-space: nowrap;
+`
+export const Input = styled.input`
+	border-radius: 3px;
+	border: 1px solid #cccccc;
+	font-size: 1.5rem;
+	padding: 4px;
+	margin-left: 1.5rem;
+	&:focus {
+		border-color: #1da0ff;
+	}
 `
 export const ButtonContainer = styled.div`
-    display: flex;
-    justify-content: center;
-    margin: 1rem 0 1rem;
+	display: flex;
+	justify-content: center;
+	margin: 1rem 0 1rem;
 `
 export const Button = styled.input`
-    padding: 0.5rem;
-    background-color: #f7f6f6;
-    /* outline: none; */
-    border: 1px solid #cccccc;
-    border-radius: 4px;
-    margin-right: 13px;
-    font-size: 1rem;
-    width: 5rem;
-    color: gray;
-    &:hover {
-        background-color: white;
-        cursor: pointer;
-    }
+	padding: 0.5rem;
+	background-color: #f7f6f6;
+	border: 1px solid #cccccc;
+	border-radius: 4px;
+	margin-right: 13px;
+	font-size: 1rem;
+	width: 5rem;
+	color: gray;
+	&:hover {
+		background-color: white;
+		cursor: pointer;
+	}
 `
 export const OpenFormIcn = styled(AddCircle)`
-    color: #003dff;
-    width: 5rem;
-    height: 5rem;
-    z-index: 100;
-    position: absolute;
-    right: 50px;
-    bottom: 50px;
-    &:hover {
-        opacity: 0.6;
-        cursor: pointer;
-    }
+	color: #003dff;
+	width: 5rem;
+	height: 5rem;
+	z-index: 100;
+	position: absolute;
+	right: 50px;
+	bottom: 50px;
+	&:hover {
+		opacity: 0.6;
+		cursor: pointer;
+	}
 `
 export const CloseFormIcn = styled(Close)`
-    color: #bfbfbf;
-    width: 2rem;
-    height: 2rem;
-    z-index: 100;
-    display: flex;
-    align-self: flex-end;
-    &:hover {
-        opacity: 0.6;
-        cursor: pointer;
-    }
+	color: #bfbfbf;
+	width: 2rem;
+	height: 2rem;
+	z-index: 100;
+	display: flex;
+	align-self: flex-end;
+	&:hover {
+		opacity: 0.6;
+		cursor: pointer;
+	}
 `
